8b84d85cddb52b7dc7f6397cc07b51239388ee97
Bryan <r0889617@student.thomasmore.be>
2022-12-14 13:19:28 +0100

    client folder test (not working)

--- code/webapp/routes/web.php
+++ code/webapp/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('test');
+    return view('index');
+});
+
+Route::get('/client', function () {
+    return view('client');
 });
