34ff677ead5353d24f37516df41fdfbf40e4484f
Bryan <r0889617@student.thomasmore.be>
2022-12-14 13:19:12 +0100

    navbar component

---
+++ code/webapp/resources/views/components/navbar.blade.php
@@ -0,0 +1,26 @@
+<div class="bg-[#222d32] w-1/6 min-h-full">
+    <a href="" class="flex justify-center text-2xl font-bold text-white bg-[#357ca5] w-full px-6 py-3">Waaiburg</a> 
+    <div>
+        <p class="text-sm text-[#4b646f] bg-[#1a2226] px-6 py-3">Gegevens</p>
+        <div class="flex flex-col text-[#b8c7ce]">
+            <a href="" class="px-6 py-3">Clienten</a>
+            <a href="" class="px-6 py-3">Begeleiders</a>
+            <a href="" class="px-6 py-3">Afdelingen</a>
+        </div>
+    </div>
+    <div>
+        <p class="text-sm text-[#4b646f] bg-[#1a2226] px-6 py-3">Informatie</p>
+        <div class="flex flex-col text-[#b8c7ce]">
+            <a href="" class="px-6 py-3">Volwassenen</a>
+            <a href="" class="px-6 py-3">Jongeren</a>
+            <a href="" class="px-6 py-3">Nieuwtjes</a>
+        </div>
+    </div>
+    <div>
+        <p class="text-sm text-[#4b646f] bg-[#1a2226] px-6 py-3">Admin</p>
+        <div class="flex flex-col text-[#b8c7ce]">
+            <a href="" class="px-6 py-3">Mails</a>
+            <a href="" class="px-6 py-3">Tevredenheids meting</a>
+        </div>
+    </div>
+</div>
