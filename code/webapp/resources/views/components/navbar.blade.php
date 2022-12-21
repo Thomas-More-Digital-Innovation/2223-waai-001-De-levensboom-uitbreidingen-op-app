efa95fc041f2be5090eb85c6cd3878c22f9da35b
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-21 14:18:33 +0100

    navbar links

--- code/webapp/resources/views/components/navbar.blade.php
+++ code/webapp/resources/views/components/navbar.blade.php
@@ -1,9 +1,9 @@
 <div class="bg-[#222d32] w-1/6 min-h-full">
-    <a href="" class="flex justify-center text-2xl font-bold text-white bg-[#357ca5] w-full px-6 py-3">Waaiburg</a> 
+    <a href="/" class="flex justify-center text-2xl font-bold text-white bg-[#357ca5] w-full px-6 py-3">Waaiburg</a> 
     <div>
         <p class="text-sm text-[#4b646f] bg-[#1a2226] px-6 py-3">Gegevens</p>
         <div class="flex flex-col text-[#b8c7ce]">
-            <a href="" class="px-6 py-3 hover:text-white hover:bg-[#202c2c]">Clienten</a>
+            <a href="/client" class="px-6 py-3 hover:text-white hover:bg-[#202c2c]">Clienten</a>
             <a href="" class="px-6 py-3 hover:text-white hover:bg-[#202c2c]">Begeleiders</a>
             <a href="" class="px-6 py-3 hover:text-white hover:bg-[#202c2c]">Afdelingen</a>
         </div>
