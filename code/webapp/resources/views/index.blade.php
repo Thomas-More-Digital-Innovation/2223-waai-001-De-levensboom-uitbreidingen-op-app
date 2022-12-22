441148e91fcd4f9cc028701a5670028c44c2cf0c
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-22 12:43:54 +0100

    dashboard design finished

--- code/webapp/resources/views/index.blade.php
+++ code/webapp/resources/views/index.blade.php
@@ -8,6 +8,7 @@
   @vite('resources/js/test.js')
   <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
 </head>
+
 <body class="flex row">
   <x-navbar />
   <main class="w-full">
@@ -15,49 +16,46 @@
       <iconify-icon icon="fa6-solid:bars" class="hover:cursor-pointer"></iconify-icon>
       <p class="hover:cursor-pointer">John Doe</p>
     </div>
-    <div class="m-5">
-      <div class="flex gap-2 items-end">
-        <h1 class="text-2xl">Dashboard</h1>
-        <p class="font-[300]">Welcome to Admin Dashboard</p>
-      </div>
 
-      <div class="grid grid-cols-3 gap-5">
+    <div class="flex gap-2 items-end ml-5 mt-5">
+      <h1 class="text-2xl">Dashboard</h1>
+      <p class="font-[300]">Welcome to Admin Dashboard</p>
+    </div>
 
-        <a class="flex rounded-sm bg-white border shadow-md md:w-[260px] h-[90px]">
-          <div class="bg-[#00a65a] ">
-            <iconify-icon icon="fa6-solid:users" class="min-w-[80px] mx-auto md:h-auto hover:cursor-pointer"></iconify-icon>
-          </div>
-          <div class="flex flex-col mx-2">
-            <h3 class="">CLIENTEN</h3>
-            <p>Amount</p>
-          </div>
-        </a>
+    <div class="grid grid-cols-3 gap-5 m-5">
+      <a href="" class="flex rounded-sm border shadow-md">
+        <iconify-icon icon="fa6-solid:users" class="bg-[#00a65a] self-center p-6 text-5xl text-white"></iconify-icon>
+        <div class="bg-white px-3 py-2 text-[#3c8dbc] font-medium">
+          <p>CLIENTEN</p>
+          <p class="font-bold text-[#3c8dbc]">AMOUNT</p>
+        </div>
+      </a>
 
-        <a class="flex rounded-sm bg-white border shadow-md md:w-[260px] h-[90px]">
-          <div class="bg-[#ff851b] object-cover min-w-[80px] md:h-auto"> Font Awesome? </div>
-          <div class="flex flex-col mx-2">
-            <h3 class="">BEGELEIDERS</h3>
-            <p>Amount</p>
-          </div>
-        </a>
+      <a href="" class="flex rounded-sm border shadow-md">
+        <iconify-icon icon="fa6-solid:address-card" class="bg-[#ff851b] self-center px-7 py-6 text-5xl text-white"></iconify-icon>
+        <div class="bg-white px-3 py-2 text-[#3c8dbc] font-medium">
+          <p>BEGELEIDERS</p>
+          <p class="font-bold text-[#3c8dbc]">AMOUNT</p>
+        </div>
+      </a>
 
-        <a class="flex rounded-sm bg-white border shadow-md md:w-[260px] h-[90px]">
-          <div class="bg-[#0073b7] object-cover min-w-[80px] md:h-auto"> Font Awesome? </div>
-          <div class="flex flex-col mx-2">
-            <h3 class="">AFDELINGEN</h3>
-            <p id="afdelingen">Amount</p>
-          </div>
-        </a>
+      <a href="" class="flex rounded-sm border shadow-md">
+        <iconify-icon icon="fa6-solid:building" class="bg-[#0073b7] self-center px-9 py-6 text-5xl text-white"></iconify-icon>
+        <div class="bg-white px-3 py-2 text-[#3c8dbc] font-medium">
+          <p>AFDELINGEN</p>
+          <p id="afdelingen" class="font-bold text-[#3c8dbc]">AMOUNT</p>
+        </div>
+      </a>
 
-        <a class="flex rounded-sm bg-white border shadow-md md:w-[260px] h-[90px]">
-          <div class="bg-[#dd4b39] object-cover min-w-[80px] md:h-auto"> Font Awesome? </div>
-          <div class="flex flex-col mx-2">
-            <h3 class="">NIEUWTJES</h3>
-            <p>Amount</p>
-          </div>
-        </a>
-      </div>
+      <a href="" class="flex rounded-sm border shadow-md">
+        <iconify-icon icon="fa6-solid:info" class="bg-[#dd4b39] self-center px-11 py-6 text-5xl text-white"></iconify-icon>
+        <div class="bg-white px-3 py-2 text-[#3c8dbc] font-medium">
+          <p>NIEUWTJES</p>
+          <p class="font-bold text-[#3c8dbc]">AMOUNT</p>
+        </div>
+      </a>
     </div>
   </main>
 </body>
+
 </html>
