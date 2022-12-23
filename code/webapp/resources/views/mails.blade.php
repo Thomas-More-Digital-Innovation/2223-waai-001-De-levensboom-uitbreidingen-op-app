bff6bb9d1bc2d84a7e1eb5ec238a33793820ad84
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-23 20:23:24 +0100

    initial design for all pages

--- code/webapp/resources/views/mails.blade.php
+++ code/webapp/resources/views/mails.blade.php
@@ -10,14 +10,23 @@
 
 <body class="flex">
   <x-navbar />
-  <main class="w-full">
+  <main class="w-full bg-[#ecf0f5]">
     <x-topbar />
     <x-welcome />
 
-    <div class="m-5">
-      <h1 class="text-3xl font-bold">
-        Mails
-      </h1>
+    <div class="m-5 bg-white rounded border">
+      <div class="border-t-4 rounded border-[#3c8dbc]">
+        <div class="m-3">
+          <div class="flex items-center justify-between">
+            <h1 class="text-2xl">Mails</h1>
+            <a href=""><iconify-icon icon="fa6-solid:plus" class="text-3xl text-[#3c8dbc]"></iconify-icon></a>
+          </div>
+          <div class="mt-5 grid grid-cols-2">
+            <p>Onderwerp</p>
+            <p>Acties</p>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </body>
