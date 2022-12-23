59d35069fd09bc3c088092e39dab80d1a4424c4d
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-23 17:35:52 +0100

    background color on index

--- code/webapp/resources/views/index.blade.php
+++ code/webapp/resources/views/index.blade.php
@@ -12,38 +12,38 @@
 
 <body class="flex">
   <x-navbar />
-  <main class="w-full">
+  <main class="w-full bg-[#ecf0f5]">
     <x-topbar />
     <x-welcome />
 
     <div class="grid grid-cols-3 gap-5 m-5">
-      <a href="/clients" class="flex rounded-sm border shadow-md">
+      <a href="/clients" class="flex rounded-sm border shadow-md bg-white">
         <iconify-icon icon="fa6-solid:users" class="bg-[#00a65a] self-center p-6 text-5xl text-white"></iconify-icon>
-        <div class="bg-white px-3 py-2 text-[#3c8dbc] font-medium">
+        <div class=" px-3 py-2 text-[#3c8dbc] font-medium">
           <p>CLIENTEN</p>
           <p class="font-bold text-[#3c8dbc]">{{ \App\Models\User::count(); }}</p>
         </div>
       </a>
       
-      <a href="/mentors" class="flex rounded-sm border shadow-md">
+      <a href="/mentors" class="flex rounded-sm border shadow-md bg-white">
         <iconify-icon icon="fa6-solid:address-card" class="bg-[#ff851b] self-center px-7 py-6 text-5xl text-white"></iconify-icon>
-        <div class="bg-white px-3 py-2 text-[#3c8dbc] font-medium">
+        <div class="px-3 py-2 text-[#3c8dbc] font-medium">
           <p>BEGELEIDERS</p>
           <p class="font-bold text-[#3c8dbc]">{{ \App\Models\User::count(); }}</p>
         </div>
       </a>
 
-      <a href="/departments" class="flex rounded-sm border shadow-md">
+      <a href="/departments" class="flex rounded-sm border shadow-md bg-white">
         <iconify-icon icon="fa6-solid:building" class="bg-[#0073b7] self-center px-9 py-6 text-5xl text-white"></iconify-icon>
-        <div class="bg-white px-3 py-2 text-[#3c8dbc] font-medium">
+        <div class="px-3 py-2 text-[#3c8dbc] font-medium">
           <p>AFDELINGEN</p>
           <p class="font-bold text-[#3c8dbc]">{{ \App\Models\Department::count(); }}</p>
         </div>
       </a>
 
-      <a href="/news" class="flex rounded-sm border shadow-md">
+      <a href="/news" class="flex rounded-sm border shadow-md bg-white">
         <iconify-icon icon="fa6-solid:info" class="bg-[#dd4b39] self-center px-11 py-6 text-5xl text-white"></iconify-icon>
-        <div class="bg-white px-3 py-2 text-[#3c8dbc] font-medium">
+        <div class="px-3 py-2 text-[#3c8dbc] font-medium">
           <p>NIEUWTJES</p>
           <p class="font-bold text-[#3c8dbc]">{{ \App\Models\Info::count(); }}</p>
         </div>
