8b5972e7ee141ed2b5ac040357dcbbe6351977fb
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-23 20:10:09 +0100

    make navbar full height

--- code/webapp/resources/views/components/navbar.blade.php
+++ code/webapp/resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-[#222d32] w-1/6 min-h-full">
+<div class="bg-[#222d32] w-1/6 h-screen">
     <a href="/" class="flex justify-center text-2xl font-bold text-white bg-[#357ca5] w-full px-6 py-3">Waaiburg</a> 
     <div>
         <p class="text-sm text-[#4b646f] bg-[#1a2226] px-6 py-3">Gegevens</p>
