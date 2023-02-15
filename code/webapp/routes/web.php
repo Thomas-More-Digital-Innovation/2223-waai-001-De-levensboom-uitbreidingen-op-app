d92e93053fa6207c58d44d2b3ce5ba890d25f594
Bryan-Deckers <r0889617@student.thomasmore.be>
2023-02-15 23:00:56 +0100

    named routes for every page

--- code/webapp/routes/web.php
+++ code/webapp/routes/web.php
@@ -29,53 +29,53 @@ Route::get('/', function () {
         'departmentcount' => $departmentcount,
         'newscount' => $newscount
     ]);
-});
+})->name('index');
 
 Route::get('/adults', function () {
     return view('adults', [
         'adults' => User::all()
     ]);
-});
+})->name('adults');
 
 Route::get('/clients', function () {
     return view('clients', [
         'clients' => User::all()
     ]);
-});
+})->name('clients');
 
 Route::get('/departments', function () {
     return view('departments', [
         'departments' => Department::all()
     ]);
-});
+})->name('departments');
 
 Route::get('/mails', function () {
     return view('mails', [
         'mails' => User::all()
     ]);
-});
+})->name('mails');
 
 Route::get('/mentors', function () {
     return view('mentors', [
         'mentors' => User::all()
     ]);
-});
+})->name('mentors');
 
 Route::get('/news', function () {
     return view('news', [
         'news' => User::all()
     ]);
-});
+})->name('news');
 
 Route::get('/surveys', function () {
     return view('surveys', [
         'surveys' => User::all()
     ]);
-});
+})->name('surveys');
 
 Route::get('/teens', function () {
     return view('teens', [
         'teens' => User::all()
     ]);
-});
+})->name('teens');
 
