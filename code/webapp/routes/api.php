733f1db92872e58b530d1f7b96f0ae67d18f65f3
Bryan <r0889617@student.thomasmore.be>
2023-02-17 14:04:56 +0100

    departmentList controller

--- code/webapp/routes/api.php
+++ code/webapp/routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Api\SectionController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\QuestionController;
 use App\Http\Controllers\Api\AnswerController;
+use App\Http\Controllers\Api\DepartmentListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -36,6 +37,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group( function () {
     Route::apiResources([
         'department' => DepartmentController::class,
+        'departmentList' => DepartmentListController::class,
         'userType' => UserTypeController::class,
         'info' => InfoController::class,
         'infoContent' => InfoContentController::class,
