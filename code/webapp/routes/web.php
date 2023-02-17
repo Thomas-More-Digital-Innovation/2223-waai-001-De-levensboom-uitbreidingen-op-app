f53c3b6a9efbe51d1b183879d53ba8ce8c231267
Siebe Camerman <88733751+SiebeCamerman@users.noreply.github.com>
2023-02-17 19:36:55 +0100

    Update web.php

--- code/webapp/routes/web.php
+++ code/webapp/routes/web.php
@@ -87,9 +87,6 @@ Route::get('/teens', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 
 Route::middleware('auth')->group(function () {
