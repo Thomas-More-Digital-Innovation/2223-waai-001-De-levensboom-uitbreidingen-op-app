e1936a7b17305cf383392a0a6dba0d1b3c315d42
Bryan-Deckers <r0889617@student.thomasmore.be>
2023-02-19 14:46:15 +0100

    client all fields with spacing

--- code/webapp/resources/views/clients/edit.blade.php
+++ code/webapp/resources/views/clients/edit.blade.php
@@ -24,57 +24,74 @@
             @method('PUT')
 
             <label for="firstname" class="font-bold">Voornaam*</label>
-            <input type="text" name="firstname" id="firstname" required placeholder="Enter voornaam" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->firstname }}>
+            <input type="text" name="firstname" id="firstname" required placeholder="Enter voornaam" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->firstname }}>
             
             <label for="surname" class="font-bold">Achternaam*</label>
-            <input type="text" name="surname" id="surname" required placeholder="Enter achternaam" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->surname }}>
+            <input type="text" name="surname" id="surname" required placeholder="Enter achternaam" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->surname }}>
 
 
             <label for="email" class="font-bold">Email*</label>
-            <input type="text" name="email" id="email" required placeholder="Enter email" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->email }}>
+            <input type="text" name="email" id="email" required placeholder="Enter email" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->email }}>
 
 
             <label for="birthdate" class="font-bold">Geboortedatum*</label>
-            <input type="text" name="birthdate" id="birthdate" required placeholder="Enter geboortedatum Vb: 1989-05-19" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->birthdate }}>
+            <input type="text" name="birthdate" id="birthdate" required placeholder="Enter geboortedatum Vb: 1989-05-19" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->birthdate }}>
 
             <label for="gender" class="font-bold">Geslacht</label>
-            <select name="gender" id="gender" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
+            <select name="gender" id="gender" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-5">
               <option value="man">Man</option>
               <option value="woman">Vrouw</option>
             </select>
 
             <hr>
-            <label for="department" class="font-bold">Afdeling</label>
-            <select name="department" id="department" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
-              @foreach ($departments as $department)
-                <option value="{{ $department->id }}">{{ $department->name }}</option>
-              @endforeach
-            </select>
+
+            <div class="flex items-center gap-3 mt-3 mb-3">
+              <label for="department" class="font-bold">Afdeling</label>
+              <iconify-icon icon="fa6-solid:plus" class="text-[#3c8dbc] text-xl cursor-pointer" />
+            </div>
+
+            <div class="flex items-center mb-5">
+              <select name="department" id="department" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
+                @foreach ($departments as $department)
+                  <option value="{{ $department->id }}">{{ $department->name }}</option>
+                @endforeach
+              </select>
+              <a href="#" class="text-[#3c8dbc] ml-2">Verwijder</a>
+            </div>
+
             <hr>
             
-            <label for="mentors" class="font-bold">Begeleiders</label>
-            {{-- select met toevoegen van meerdere --}}
-            <select name="mentors" id="mentors" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
-              @foreach ($mentors as $mentor)
-                <option value="{{ $mentor->id }}">{{ $mentor->firstname }} {{ $mentor->surname }}</option>
-              @endforeach
-            </select>
+            <div class="flex items-center gap-3 mt-3 mb-2">
+              <label for="mentors" class="font-bold">Begeleiders</label>
+              <iconify-icon icon="fa6-solid:plus" class="text-[#3c8dbc] text-xl cursor-pointer" />
+            </div>
+
+            <div class="flex items-center mb-5">
+              <select name="mentors" id="mentors" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
+                @foreach ($mentors as $mentor)
+                  <option value="{{ $mentor->id }}">{{ $mentor->firstname }} {{ $mentor->surname }}</option>
+                @endforeach
+              </select>
+              <a href="#" class="text-[#3c8dbc] ml-2">Verwijder</a>
+            </div>
+            
             <hr>
-            <p class="mt-5 text-lg">Contactgegevens &lpar;optioneel&rpar;</p>
+            <p class="mt-5 text-lg mb-3">Contactgegevens &lpar;optioneel&rpar;</p>
             <label for="street" class="font-bold">Straat</label>
-            <input type="text" name="street" id="street" placeholder="Enter straat" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->street }}>
+            <input type="text" name="street" id="street" placeholder="Enter straat" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->street }}>
 
             <label for="houseNumber" class="font-bold">Huis nummer</label>
-            <input type="text" name="houseNumber" id="houseNumber" placeholder="Enter huis nummer" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->houseNumber }}>
+            <input type="text" name="houseNumber" id="houseNumber" placeholder="Enter huis nummer" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->houseNumber }}>
 
             <label for="city" class="font-bold">Woonplaats</label>
-            <input type="text" name="city" id="city" placeholder="Enter woonplaats" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->city }}>
+            <input type="text" name="city" id="city" placeholder="Enter woonplaats" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->city }}>
 
             <label for="zipcode" class="font-bold">Postcode</label>
-            <input type="text" name="zipcode" id="zipcode" placeholder="Enter postcode" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->zipcode }}>
+            <input type="text" name="zipcode" id="zipcode" placeholder="Enter postcode" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->zipcode }}>
 
             <label for="phoneNumber" class="font-bold">Telefoonnummer</label>
-            <input type="text" name="phoneNumber" id="phoneNumber" placeholder="Enter telefoonnummer" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]" value={{ $client->phoneNumber }}>
+            <input type="text" name="phoneNumber" id="phoneNumber" placeholder="Enter telefoonnummer" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $client->phoneNumber }}>
+            
             <button type="submit" class="bg-[#3c8dbc] rounded mr-auto px-4 py-1 mt-5 text-white">Wijzigen</button>
         </form>
         </div>
