00c41643c88bbaf7aa7fb21f950fddfa4d2978b5
Bryan-Deckers <r0889617@student.thomasmore.be>
2023-02-19 15:47:00 +0100

    edits changed from put to patch

--- code/webapp/resources/views/mentors/edit.blade.php
+++ code/webapp/resources/views/mentors/edit.blade.php
@@ -20,7 +20,7 @@
             <h1 class="text-2xl">Begeleider wijzigen</h1>
             <form action="{{ route('mentors.update', $mentor->id) }}" method="POST" class="flex flex-col mt-3">
                 @csrf
-                @method('PUT')
+                @method('PATCH')
     
                 <label for="firstname" class="font-bold">Voornaam*</label>
                 <input type="text" name="firstname" id="firstname" placeholder="Enter voornaam" required class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $mentor->firstname }}>
