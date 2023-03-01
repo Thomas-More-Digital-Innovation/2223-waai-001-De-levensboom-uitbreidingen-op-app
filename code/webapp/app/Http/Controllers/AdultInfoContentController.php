92a768eb448368eb1699460016bbd9413bafe365
Bryan <r0889617@student.thomasmore.be>
2023-03-01 15:33:43 +0100

    fix error

--- code/webapp/app/Http/Controllers/AdultInfoContentController.php
+++ code/webapp/app/Http/Controllers/AdultInfoContentController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Info;
 use App\Models\InfoContent;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Gate;
 
