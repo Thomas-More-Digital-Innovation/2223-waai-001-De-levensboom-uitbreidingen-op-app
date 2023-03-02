999f7a86e042d3fe2b16111d1ac24b4267b04f95
Sens van Aert <r0878915@student.thomasmore.be>
2023-03-02 11:38:57 +0100

    improved foreach in edit mentors

--- code/webapp/resources/views/mentors/edit.blade.php
+++ code/webapp/resources/views/mentors/edit.blade.php
@@ -30,12 +30,14 @@
                 <hr/>
 
                 <div id="dropdowns">
-                  <div id="0" class="flex flex-row gap-5">
-                    <div>
-                      <div class="flex items-center gap-3 mt-3 mb-3">
-                        <label for="role0" class="font-bold">Functie*</label>
-                      </div>
-                      @foreach ($departmentsList as $departmentList)
+                  @foreach ($departmentsList as $departmentList)
+                    <div id="{{ $loop->index }}" class="flex flex-row gap-5">
+                      <div>
+                        @if ( $loop->index == 0)
+                          <div class="flex items-center gap-3 mt-3 mb-3">
+                            <label for="role0" class="font-bold">Functie*</label>
+                          </div>
+                        @endif
                         <div class="flex items-center">
                           <select name="role{{ $loop->index }}" id="role{{ $loop->index }}" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3">
                             <option value="">Kies een Functie</option>
@@ -50,14 +52,14 @@
                             @endforeach
                           </select>
                         </div>
-                      @endforeach
-                    </div>
-                    <div>
-                      <div class="flex items-center gap-3 mt-3 mb-3">
-                        <label for="department0" class="font-bold">Afdeling</label>
-                        <iconify-icon icon="fa6-solid:plus" class="text-[#3c8dbc] text-xl cursor-pointer" onclick="addDepartment()" />
                       </div>
-                      @foreach ($departmentsList as $departmentList)
+                      <div>
+                        @if ( $loop->index == 0 )
+                          <div class="flex items-center gap-3 mt-3 mb-3">
+                            <label for="department0" class="font-bold">Afdeling</label>
+                            <iconify-icon icon="fa6-solid:plus" class="text-[#3c8dbc] text-xl cursor-pointer" onclick="addDepartment()" />
+                          </div>
+                        @endif
                         <div class="flex items-center mb-3">
                           <select name="department{{ $loop->index }}" id="department{{ $loop->index }}" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
                             <option value="">Kies een Afdeling</option>
@@ -71,11 +73,13 @@
                               </option>
                             @endforeach
                           </select>
+                          @if ( $loop->index != 0 )
                           <button onclick="deleteDepartment( '0' )" class="text-[#3c8dbc] ml-2">Verwijder</button>
+                          @endif
                         </div>
-                      @endforeach
+                      </div>
                     </div>
-                  </div>
+                  @endforeach
                 </div>
 
                 <input name="totalDep" id="totalDep" value="1" class="hidden" />
