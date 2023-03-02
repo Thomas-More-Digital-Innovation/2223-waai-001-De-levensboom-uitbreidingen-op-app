5ed180debaad0cdbdebdc8d50a7fe55f96e20523
Bryan <r0889617@student.thomasmore.be>
2023-03-02 12:28:23 +0100

    user seeder

---
+++ code/webapp/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user')->insert([
+            'user_type_id' => 1,
+            'firstname' => 'Admin',
+            'surname' => 'Admin',
+            'birthdate' => '1990-01-01',
+            'email' => 'admin@dewaaiburgapp.eu',
+            'password' => bcrypt('adminadmin'),
+        ]);
+    }
+}
