1252fe6516769b83789cc2f38e60d12155203ce0
Bryan <r0889617@student.thomasmore.be>
2023-03-06 11:05:56 +0100

    hover naam rechtsboven

--- code/webapp/resources/views/components/topbar.blade.php
+++ code/webapp/resources/views/components/topbar.blade.php
@@ -1,7 +1,7 @@
 @vite('resources/css/app.css')
 <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
-<div class="min-h-[56px] bg-[#3c8dbc] flex justify-end items-center px-5 text-white">
-    <button onclick="document.getElementById('dropdown').classList.toggle('hidden');" class="text-white hover:bg-gray-200 block " type="button">{{ $user->firstname . ' ' . $user->surname}}</button>
+<div class="min-h-[56px] bg-[#3c8dbc] flex justify-end items-center text-white">
+    <button onclick="document.getElementById('dropdown').classList.toggle('hidden');" class="text-white hover:bg-[#337ab7] block p-4" type="button">{{ $user->firstname . ' ' . $user->surname}}</button>
     <!-- Dropdown menu -->
     <div id="dropdown" class="z-10 absolute right-0 mx-5 w-52 top-16 shadow-lg bg-white rounded-md border-gray-500 border hidden">
         <ul class="py-2 text-sm text-black " aria-labelledby="dropdownDefaultButton">
