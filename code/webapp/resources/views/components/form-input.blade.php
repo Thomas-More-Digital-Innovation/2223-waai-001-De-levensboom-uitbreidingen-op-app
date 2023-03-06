5f5a5f5dbd2a3675c8d7047d1ac2145cd6df47da
Bryan <r0889617@student.thomasmore.be>
2023-03-06 13:05:46 +0100

    input required fix

--- code/webapp/resources/views/components/form-input.blade.php
+++ code/webapp/resources/views/components/form-input.blade.php
@@ -1,6 +1,6 @@
 <label for="{{ $name }}" class="font-bold">{{$text}}{{ $required === "true" ? "*" : "" }}</label>
 @if ($value != null)
-    <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" required {{$disabled}} placeholder="Enter {{ strtolower($text) }}" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3 @error("{{ $name }}") is-invalid @enderror" value="{{ $value->$name }}">
+    <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" {{ $required === "true" ? "required" : null }} {{$disabled}} placeholder="Enter {{ strtolower($text) }}" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3 @error("{{ $name }}") is-invalid @enderror" value="{{ $value->$name }}">
 @else 
-    <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" required {{$disabled}} placeholder="Enter {{ strtolower($text) }}" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3 @error("{{ $name }}") is-invalid @enderror" value="{{old($name)}}">
+    <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" {{ $required === "true" ? "required" : null }} {{$disabled}} placeholder="Enter {{ strtolower($text) }}" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3 @error("{{ $name }}") is-invalid @enderror" value="{{old($name)}}">
 @endif
