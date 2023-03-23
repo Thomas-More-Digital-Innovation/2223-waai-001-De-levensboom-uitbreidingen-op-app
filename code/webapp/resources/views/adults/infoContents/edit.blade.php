18f89b944cb23f8bc378afe5e4bd068b298f7ab1
Sens van Aert <r0878915@student.thomasmore.be>
2023-03-23 16:43:39 +0100

    Did the same for edit

--- code/webapp/resources/views/adults/infoContents/edit.blade.php
+++ code/webapp/resources/views/adults/infoContents/edit.blade.php
@@ -30,7 +30,7 @@
 
                         <x-form-input name="title" text="Titel" :value="$infoContent" />
 
-                        <label for="titleImage" class="font-bold">Blok Foto</label>
+                        <label for="titleImageUrl" class="font-bold">Blok Foto</label>
                         <p>Geef een url in van een foto die online staat, of upload een foto van je op pc.</p>
                         <input type="text" name="titleImageUrl" id="titleImageUrl" placeholder="Enter blok foto url"
                             class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3"
