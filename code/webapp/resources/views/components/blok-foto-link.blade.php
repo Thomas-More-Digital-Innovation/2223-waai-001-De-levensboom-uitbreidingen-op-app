a321c02935ab7386dcb18a8198cf9aada5d0f79e
Bryan Deckers <r0889617@student.thomasmore.be>
2023-03-24 13:17:00 +0100

    spacing and new line

--- code/webapp/resources/views/components/blok-foto-link.blade.php
+++ code/webapp/resources/views/components/blok-foto-link.blade.php
@@ -1,8 +1,6 @@
 <label for="titleImageUrl" class="font-bold">Blok Foto</label>
 <p>Geef een url in van een foto die online staat, of upload een foto van je op pc.</p>
 <input type="text" name="titleImageUrl" id="titleImageUrl" placeholder="Enter blok foto url"
-    class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3"
-    value={{ $url }}>
-<input type="file" name="titleImage" id="titleImage"
-    class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3"
+    class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3" value={{ $url }}>
+<input type="file" name="titleImage" id="titleImage" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-3"
     value={{ $url }}>
