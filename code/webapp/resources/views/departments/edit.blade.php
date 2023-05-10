b71f57e79fa8157491847eea520d32369445eafc
Bryan <r0889617@student.thomasmore.be>
2023-05-10 10:33:40 +0200

    afdeling edit use layout

--- code/webapp/resources/views/departments/edit.blade.php
+++ code/webapp/resources/views/departments/edit.blade.php
@@ -1,43 +1,20 @@
-<!doctype html>
-<html lang="nl">
+@extends('layout')
+<title>Waaiburg - Afdelingen</title>
+@section('content')
+    <h1 class="text-2xl">Afdeling wijzigen</h1>
+    <form action="{{ route('departments.update', $department->id) }}" method="POST" class="flex flex-col mt-3">
+        @csrf
+        @method('PATCH')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>Waaiburg - Afdelingen</title>
-</head>
+        <x-errormessage />
 
-<body class="flex">
-    <x-navbar />
-    <main class="w-full bg-[#ecf0f5]">
-        <x-topbar />
-        <x-welcome />
+        <x-form-input name="name" text="Naam" :value="$department" />
 
-        <div class="m-5 bg-white rounded border">
-            <div class="border-t-4 rounded border-[#3c8dbc]">
-                <div class="m-3">
-                    <h1 class="text-2xl">Afdeling wijzigen</h1>
-                    <form action="{{ route('departments.update', $department->id) }}" method="POST"
-                        class="flex flex-col mt-3">
-                        @csrf
-                        @method('PATCH')
-
-                        <x-errormessage />
-
-                        <x-form-input name="name" text="Naam" :value="$department" />
-
-                        <x-contactgegevens :contactgegevens="$department" />
-                        <x-form-input name="email" text="Email" :value="$department" />
-                        <div class="flex gap-5">
-                            <x-form-button text="Bewerk" />
-                            <x-form-button text="Annuleren" link="departments.index" />
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <x-contactgegevens :contactgegevens="$department" />
+        <x-form-input name="email" text="Email" :value="$department" />
+        <div class="flex gap-5">
+            <x-form-button text="Bewerk" />
+            <x-form-button text="Annuleren" link="departments.index" />
         </div>
-    </main>
-</body>
-
-</html>
+    </form>
+@endsection
