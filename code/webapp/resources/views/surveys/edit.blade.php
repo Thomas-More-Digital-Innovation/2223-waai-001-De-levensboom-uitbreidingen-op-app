dfdb75f0dcef13185192cd25d167c9cf0016fe9b
Bryan <r0889617@student.thomasmore.be>
2023-05-10 10:24:29 +0200

    survey use layout

--- code/webapp/resources/views/surveys/edit.blade.php
+++ code/webapp/resources/views/surveys/edit.blade.php
@@ -1,40 +1,18 @@
-<!doctype html>
-<html lang="nl">
+@extends('layout')
+<title>Waaiburg - Tevredenheids Meting</title>
+@section('content')
+    <h1 class="text-2xl">Tevredenheids meting wijzigen</h1>
+    <form action="{{ route('surveys.update', $survey->id) }}" method="POST" class="flex flex-col mt-3">
+        @csrf
+        @method('PATCH')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>Waaiburg - Tevredenheids Meting</title>
-</head>
+        <x-errormessage />
 
-<body class="flex">
-    <x-navbar />
-    <main class="w-full bg-[#ecf0f5]">
-        <x-topbar />
-        <x-welcome />
+        <x-form-input name="url" text="Google form link" :value="$survey" />
 
-        <div class="m-5 bg-white rounded border">
-            <div class="border-t-4 rounded border-[#3c8dbc]">
-                <div class="m-3">
-                    <h1 class="text-2xl">Tevredenheids meting wijzigen</h1>
-                    <form action="{{ route('surveys.update', $survey->id) }}" method="POST" class="flex flex-col mt-3">
-                        @csrf
-                        @method('PATCH')
-
-                        <x-errormessage />
-
-                        <x-form-input name="url" text="Google form link" :value="$survey" />
-
-                        <div class="flex gap-5">
-                            <x-form-button text="Wijzigen" />
-                            <x-form-button text="Annuleren" link="surveys.index" />
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div class="flex gap-5">
+            <x-form-button text="Wijzigen" />
+            <x-form-button text="Annuleren" link="surveys.index" />
         </div>
-    </main>
-</body>
-
-</html>
+    </form>
+@endsection
--- code/webapp/resources/views/surveys/index.blade.php
+++ code/webapp/resources/views/surveys/index.blade.php
@@ -1,48 +1,27 @@
-<!doctype html>
-<html lang="nl">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>Waaiburg - Tevredenheids Meting</title>
-</head>
-
-<body class="flex">
-    <x-navbar />
-    <main class="w-full bg-[#ecf0f5]">
-        <x-topbar />
-        <x-welcome />
-
-        <div class="m-5 bg-white rounded border">
-            <div class="border-t-4 rounded border-[#3c8dbc]">
-                <div class="m-3">
-                    <x-list-title title="Tevredenheids Meting" name="" />
-                    <table class="border-collapse border border-[#f4f4f4] w-full" aria-describedby="survey">
-                        <thead>
-                            <tr>
-                                <th class="border border-[#f4f4f4] py-2 px-6 text-left">Google form link</th>
-                                <th class="border border-[#f4f4f4] py-2 px-6">Acties</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($surveys as $survey)
-                                <tr class="font-normal">
-                                    <td class="border border-[#f4f4f4] py-2 px-6 w-full">{{ $survey->url }}</td>
-                                    <td class="border border-[#f4f4f4] py-2 px-6 w-full">
-                                        <a href="{{ route('surveys.edit', $survey->id) }}"
-                                            class="text-[#3c8dbc]">Bewerk</a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-        <x-documentation-link link="/De_Waaiburg_webapp_documentatie.pdf#page=18"
-            text="documentatie over tevredenheids meting" />
-    </main>
-</body>
-
-</html>
+@extends('layout')
+<title>Waaiburg - Tevredenheids Meting</title>
+@section('content')
+    <x-list-title title="Tevredenheids Meting" name="" />
+    <table class="border-collapse border border-[#f4f4f4] w-full" aria-describedby="survey">
+        <thead>
+            <tr>
+                <th class="border border-[#f4f4f4] py-2 px-6 text-left">Google form link</th>
+                <th class="border border-[#f4f4f4] py-2 px-6">Acties</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($surveys as $survey)
+                <tr class="font-normal">
+                    <td class="border border-[#f4f4f4] py-2 px-6 w-full">{{ $survey->url }}</td>
+                    <td class="border border-[#f4f4f4] py-2 px-6 w-full">
+                        <a href="{{ route('surveys.edit', $survey->id) }}" class="text-[#3c8dbc]">Bewerk</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
+@section('documentation')
+    <x-documentation-link link="/De_Waaiburg_webapp_documentatie.pdf#page=18"
+        text="documentatie over tevredenheids meting" />
+@endsection
