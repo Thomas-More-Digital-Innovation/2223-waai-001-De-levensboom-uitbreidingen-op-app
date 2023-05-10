6d442a755d10bafeb86b2eafbe90b3e160f73f8c
Bryan <r0889617@student.thomasmore.be>
2023-05-10 10:13:32 +0200

    departments use layout

--- code/webapp/resources/views/departments/create.blade.php
+++ code/webapp/resources/views/departments/create.blade.php
@@ -1,42 +1,20 @@
-<!doctype html>
-<html lang="nl">
+@extends('layout')
+<title>Waaiburg - Afdelingen</title>
+@section('content')
+    <h1 class="text-2xl">Afdeling toevoegen</h1>
+    <form action="{{ route('departments.store') }}" method="POST" class="flex flex-col mt-3">
+        @csrf
+        @method('POST')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>Waaiburg - Afdelingen</title>
-</head>
+        <x-errormessage />
 
-<body class="flex">
-    <x-navbar />
-    <main class="w-full bg-[#ecf0f5]">
-        <x-topbar />
-        <x-welcome />
+        <x-form-input name="name" text="Naam" />
 
-        <div class="m-5 bg-white rounded border">
-            <div class="border-t-4 rounded border-[#3c8dbc]">
-                <div class="m-3">
-                    <h1 class="text-2xl">Afdeling toevoegen</h1>
-                    <form action="{{ route('departments.store') }}" method="POST" class="flex flex-col mt-3">
-                        @csrf
-                        @method('POST')
-
-                        <x-errormessage />
-
-                        <x-form-input name="name" text="Naam" />
-
-                        <x-contactgegevens />
-                        <x-form-input name="email" text="Email" required="false" />
-                        <div class="flex gap-5">
-                            <x-form-button text="Aanmaken" />
-                            <x-form-button text="Annuleren" link="departments.index" />
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <x-contactgegevens />
+        <x-form-input name="email" text="Email" required="false" />
+        <div class="flex gap-5">
+            <x-form-button text="Aanmaken" />
+            <x-form-button text="Annuleren" link="departments.index" />
         </div>
-    </main>
-</body>
-
-</html>
+    </form>
+@endsection
