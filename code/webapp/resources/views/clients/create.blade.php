a8f7a9f227af8261700fa64f4d0827e0808e2b55
Bryan <r0889617@student.thomasmore.be>
2023-05-10 10:13:23 +0200

    clients use layout

--- code/webapp/resources/views/clients/create.blade.php
+++ code/webapp/resources/views/clients/create.blade.php
@@ -1,98 +1,73 @@
-<!doctype html>
-<html lang="nl">
+@extends('layout')
+<title>Waaiburg - Clienten</title>
+@section('content')
+    <h1 class="text-2xl">Client toevoegen</h1>
+    <form action="{{ route('clients.store') }}" method="POST" class="flex flex-col mt-3">
+        @csrf
+        @method('POST')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"
-        integrity="sha384-Wg6YZl1ug3L+m2P1dI9UyM3bbDxm861GXqDX7y1TetknKz8/0AoMTJT0Ktlm2Tgi" crossorigin="anonymous">
-    </script>
-    <title>Waaiburg - Clienten</title>
-</head>
+        <x-errormessage />
 
-<body class="flex relative">
-    <x-navbar />
-    <main class="w-full bg-[#ecf0f5]">
-        <x-topbar />
-        <x-welcome />
-
-        <div class="m-5 bg-white rounded border">
-            <div class="border-t-4 rounded border-[#3c8dbc]">
-                <div class="m-3">
-                    <h1 class="text-2xl">Client toevoegen</h1>
-                    <form action="{{ route('clients.store') }}" method="POST" class="flex flex-col mt-3">
-                        @csrf
-                        @method('POST')
-
-                        <x-errormessage />
-
-                        <x-form-input name="firstname" text="Voornaam" />
-                        <x-form-input name="surname" text="Achternaam" />
-                        <x-form-input name="email" text="Email" type="email" />
-                        <x-form-input name="birthdate" text="Geboortedatum" type="date" required="false" />
+        <x-form-input name="firstname" text="Voornaam" />
+        <x-form-input name="surname" text="Achternaam" />
+        <x-form-input name="email" text="Email" type="email" />
+        <x-form-input name="birthdate" text="Geboortedatum" type="date" required="false" />
 
 
-                        <label for="gender" class="font-bold">Geslacht</label>
-                        <select name="gender" id="gender"
-                            class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-5">
-                            <option value="man">Man</option>
-                            <option value="woman">Vrouw</option>
+        <label for="gender" class="font-bold">Geslacht</label>
+        <select name="gender" id="gender" class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc] mb-5">
+            <option value="man">Man</option>
+            <option value="woman">Vrouw</option>
+        </select>
+        <hr>
+
+        <div id='dropdowns'>
+            <div id='0'>
+                <div class="flex flex-row gap-5">
+                    <div>
+                        <div class="flex items-center gap-3 mt-3 mb-3">
+                            <label for="department0" class="font-bold">Afdeling</label>
+                        </div>
+                        <select name="department0" id="department0"
+                            onchange="getDepartments({{ $departmentLists }}, {{ $mentors }}, 'department0', 'mentor0')"
+                            class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
+                            <option value="">Kies een Afdeling</option>
+                            @foreach ($departments as $department)
+                                <option value="{{ $department->id }}">{{ $department->name }}</option>
+                            @endforeach
                         </select>
-                        <hr>
+                    </div>
 
-                        <div id='dropdowns'>
-                            <div id='0'>
-                                <div class="flex flex-row gap-5">
-                                    <div>
-                                        <div class="flex items-center gap-3 mt-3 mb-3">
-                                            <label for="department0" class="font-bold">Afdeling</label>
-                                        </div>
-                                        <select name="department0" id="department0"
-                                            onchange="getDepartments({{ $departmentLists }}, {{ $mentors }}, 'department0', 'mentor0')"
-                                            class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
-                                            <option value="">Kies een Afdeling</option>
-                                            @foreach ($departments as $department)
-                                                <option value="{{ $department->id }}">{{ $department->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-
-                                    <div>
-                                        <div class="flex items-center gap-3 mt-3 mb-3">
-                                            <label for="mentor0" class="font-bold">Begeleiders</label>
-                                            <iconify-icon icon="fa6-solid:plus"
-                                                class="text-[#3c8dbc] text-xl cursor-pointer"
-                                                onclick="addDepartment()" />
-                                        </div>
-                                        <div class="flex items-center mb-3">
-                                            <select name="mentor0" id="mentor0"
-                                                class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
-                                                <option value="">Kies een Begeleider</option>
-                                                @foreach ($mentors as $mentor)
-                                                    <option value="{{ $mentor->id }}">{{ $mentor->firstname }}
-                                                        {{ $mentor->surname }}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <div>
+                        <div class="flex items-center gap-3 mt-3 mb-3">
+                            <label for="mentor0" class="font-bold">Begeleiders</label>
+                            <iconify-icon icon="fa6-solid:plus" class="text-[#3c8dbc] text-xl cursor-pointer"
+                                onclick="addDepartment()" />
                         </div>
-
-                        <input name="totalDep" id="totalDep" value="1" class="hidden" />
-                        <hr>
-                        <x-contactgegevens />
-                        <div class="flex gap-5">
-                            <x-form-button text="Aanmaken" />
-                            <x-form-button text="Annuleren" link="clients.index" />
+                        <div class="flex items-center mb-3">
+                            <select name="mentor0" id="mentor0"
+                                class="border border-[#d2d6de] px-4 py-2 outline-[#3c8dbc]">
+                                <option value="">Kies een Begeleider</option>
+                                @foreach ($mentors as $mentor)
+                                    <option value="{{ $mentor->id }}">{{ $mentor->firstname }}
+                                        {{ $mentor->surname }}</option>
+                                @endforeach
+                            </select>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </main>
-</body>
+
+        <input name="totalDep" id="totalDep" value="1" class="hidden" />
+        <hr>
+        <x-contactgegevens />
+        <div class="flex gap-5">
+            <x-form-button text="Aanmaken" />
+            <x-form-button text="Annuleren" link="clients.index" />
+        </div>
+    </form>
+@endsection
 
 <script>
     let nrOfDep = 1;
@@ -167,5 +142,3 @@
         dropdowns.removeChild(document.getElementById(departmentId));
     }
 </script>
-
-</html>
