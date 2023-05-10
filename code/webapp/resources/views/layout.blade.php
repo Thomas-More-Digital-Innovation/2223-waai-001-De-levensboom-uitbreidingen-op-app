bbe6cd155b31e26c1706d6c8d4ec88818b2055f5
Bryan <r0889617@student.thomasmore.be>
2023-05-10 10:03:30 +0200

    make initial layout file

---
+++ code/webapp/resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    @include('head')
+</head>
+
+<body class="flex">
+    <x-navbar />
+    <main class="w-full bg-[#ecf0f5]">
+        <x-topbar />
+        <x-welcome />
+
+        <div class="m-5 bg-white rounded border">
+            <div class="border-t-4 rounded border-[#3c8dbc]">
+                <div class="m-3">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
