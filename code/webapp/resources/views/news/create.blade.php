0bc5e5bcd3c5ab088a086811ee8a3afad0201a94
Bryan <r0889617@student.thomasmore.be>
2023-05-10 10:03:41 +0200

    use layout and head files in news create

--- code/webapp/resources/views/news/create.blade.php
+++ code/webapp/resources/views/news/create.blade.php
@@ -1,47 +1,23 @@
-<!doctype html>
-<html lang="nl">
+@extends('layout')
+<title>Waaiburg - Nieuwtjes</title>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>Waaiburg - Nieuwtjes</title>
-    <script src="https://cdn.ckeditor.com/4.20.2/full/ckeditor.js"
-        integrity="sha384-YfSzYL1sDylXi5TVFdhLEG6HcUdHjsjPeJ+6yUTybeVgczFkdEDx71I0UsKfDsRa" crossorigin="anonymous">
-    </script>
-</head>
+@section('content')
+    <h1 class="text-2xl">Nieuwtje toevoegen</h1>
+    <form action="{{ route('news.store') }}" method="POST" class="flex flex-col mt-3">
+        @csrf
+        @method('POST')
 
-<body class="flex">
-    <x-navbar />
-    <main class="w-full bg-[#ecf0f5]">
-        <x-topbar />
-        <x-welcome />
+        <x-errormessage />
 
-        <div class="m-5 bg-white rounded border">
-            <div class="border-t-4 rounded border-[#3c8dbc]">
-                <div class="m-3">
-                    <h1 class="text-2xl">Nieuwtje toevoegen</h1>
-                    <form action="{{ route('news.store') }}" method="POST" class="flex flex-col mt-3">
-                        @csrf
-                        @method('POST')
+        <x-form-input name="title" text="Titel" />
+        <x-form-input name="shortContent" text="Korte inhoud" required=false />
 
-                        <x-errormessage />
+        <label for="content" class="font-bold">Inhoud</label>
+        <textarea class="ckeditor form-control" name="content" id="content"></textarea>
 
-                        <x-form-input name="title" text="Titel" />
-                        <x-form-input name="shortContent" text="Korte inhoud" required=false />
-
-                        <label for="content" class="font-bold">Inhoud</label>
-                        <textarea class="ckeditor form-control" name="content" id="content"></textarea>
-
-                        <div class="flex gap-5">
-                            <x-form-button text="Aanmaken" />
-                            <x-form-button text="Annuleren" link="news.index" />
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div class="flex gap-5">
+            <x-form-button text="Aanmaken" />
+            <x-form-button text="Annuleren" link="news.index" />
         </div>
-    </main>
-</body>
-
-</html>
+    </form>
+@endsection
