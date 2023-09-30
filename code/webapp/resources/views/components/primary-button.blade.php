926f3219650901b544204b7f274c101f6b69a32f
Bryan Deckers <r0889617@student.thomasmore.be>
2023-09-30 18:28:11 +0200

    Revert "use wb blue on log in button"
    This reverts commit 53815261a4e7e7a0fe6ee0427050556d41ce3c1a.

--- code/webapp/resources/views/components/primary-button.blade.php
+++ code/webapp/resources/views/components/primary-button.blade.php
@@ -1,4 +1,4 @@
 <button
-    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-wb-blue border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-wb-blue focus:bg-wb-blue active:bg-wb-blue focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-wb-blue border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-[#0073b7] focus:bg-[#0073b7] active:bg-[#0073b7] focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
