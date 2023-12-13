455631779bcab32d0e18412251682bbae525e944
Bryan Deckers <r0889617@student.thomasmore.be>
2023-12-13 12:21:49 +0100

    Error messages positions

--- code/webapp/resources/views/auth/login.blade.php
+++ code/webapp/resources/views/auth/login.blade.php
@@ -5,31 +5,32 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
+        <x-input-error :messages="$errors->get('email')" class="my-2" />
+        <x-input-error :messages="$errors->get('password')" class="my-2" />
+
         <!-- Email Address -->
         <div class="flex flex-col">
             <x-form-input name="email" text="Email" type="email" />
-
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="flex flex-col">
             <x-form-input name="password" text="Wachtwoord" type="password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-gray-600 shadow-sm focus:ring-gray-500" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-gray-600 shadow-sm focus:ring-gray-500" name="remember">
                 <span class="ml-2 text-sm text-gray-600">Onthou mij</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                    href="{{ route('password.request') }}">
                     Wachtwoord vergeten?
                 </a>
             @endif
