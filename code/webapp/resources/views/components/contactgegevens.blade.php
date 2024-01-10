1c4c8304764446b33d994ee58a75b87df869a43a
Sens van Aert <r0878915@student.thomasmore.be>
2024-01-10 08:31:45 +0100

    Edit contactgegevens doesn't show  content after spaces fix

--- code/webapp/resources/views/components/contactgegevens.blade.php
+++ code/webapp/resources/views/components/contactgegevens.blade.php
@@ -2,23 +2,23 @@
     <p class="mt-5 text-lg mb-3">Contactgegevens &lpar;optioneel&rpar;</p>
     <label for="street" class="font-bold">Straat</label>
     <input type="text" name="street" id="street" placeholder="Geef je straat in"
-        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value={{ $contactgegevens->street }}>
+        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value="{{ $contactgegevens->street }}">
 
     <label for="houseNumber" class="font-bold">Huis nummer</label>
     <input type="text" name="houseNumber" id="houseNumber" placeholder="Geef je huis nummer in"
-        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value={{ $contactgegevens->houseNumber }}>
+        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value="{{ $contactgegevens->houseNumber }}">
 
     <label for="city" class="font-bold">Woonplaats</label>
     <input type="text" name="city" id="city" placeholder="Geef je woonplaats in"
-        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value={{ $contactgegevens->city }}>
+        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value="{{ $contactgegevens->city }}">
 
     <label for="zipcode" class="font-bold">Postcode</label>
     <input type="text" name="zipcode" id="zipcode" placeholder="Geef je postcode in"
-        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value={{ $contactgegevens->zipcode }}>
+        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value="{{ $contactgegevens->zipcode }}">
 
     <label for="phoneNumber" class="font-bold">Telefoonnummer</label>
     <input type="text" name="phoneNumber" id="phoneNumber" placeholder="Geef je telefoonnummer in"
-        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value={{ $contactgegevens->phoneNumber }}>
+        class="border border-[#d2d6de] px-4 py-2 outline-wb-blue mb-3" value="{{ $contactgegevens->phoneNumber }}">
 @else
     <p class="mt-5 text-lg mb-3">Contactgegevens &lpar;optioneel&rpar;</p>
     <label for="street" class="font-bold">Straat</label>
