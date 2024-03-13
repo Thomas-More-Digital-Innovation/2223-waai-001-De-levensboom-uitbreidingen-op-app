690783a0ab55a80b102041b1a57e60dd5ac8676a
Bryan <r0889617@student.thomasmore.be>
2024-03-13 11:18:27 +0100

    Filter clients by department

--- code/webapp/app/Http/Controllers/ClientController.php
+++ code/webapp/app/Http/Controllers/ClientController.php
@@ -25,29 +25,34 @@ class ClientController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         Gate::authorize("notClient");
 
-        $clients = User::where("user_type_id", 2)->get();
-        $departmentLists = DepartmentList::whereIn('user_id', $clients->pluck('id'))->get();
+        $departmentId = $request->input('department_id');
+
+        if ($departmentId) {
+            // Filter department list based on the selected department
+            $filteredDepartmentLists = DepartmentList::where('department_id', $departmentId)->get();
+
+            // Get the user IDs from the filtered department list
+            $userIds = $filteredDepartmentLists->pluck('user_id');
+
+            // Query users based on the filtered user IDs
+            $clients = User::whereIn('id', $userIds)->where("user_type_id", 2)->get();
+        } else {
+            // If no department selected, fetch all clients with user_type_id 2
+            $clients = User::where("user_type_id", 2)->get();
+        }
+
         $departments = Department::all();
+        $departmentLists = DepartmentList::whereIn('user_id', $clients->pluck('id'))->get();
         $userLists = UserList::whereIn('client_id', $clients->pluck('id'))->get();
         $mentors = User::where("user_type_id", 1)
             ->orWhere("user_type_id", 3)
             ->get();
-            
 
-        return view(
-            "clients.index",
-            compact(
-                "clients",
-                "departments",
-                "departmentLists",
-                "mentors",
-                "userLists"
-            )
-        );
+        return view("clients.index", compact("clients", "departments", "departmentLists", "mentors", "userLists"));
     }
 
     /**
@@ -85,7 +90,7 @@ class ClientController extends Controller
         $user = User::create($request->all());
 
         event(new Registered($user));
-        $user->notify(new CreateAccountApp(env('APP_URL')."/user#Pass"));
+        $user->notify(new CreateAccountApp(env('APP_URL') . "/user#Pass"));
 
         for ($i = 0; $i <= $request->totalDep; $i++) {
             $department = $request->input("department" . $i);
@@ -156,7 +161,7 @@ class ClientController extends Controller
         $survey = $request->input('survey_id');
         $url = InfoContent::find($survey)->first()->url;
         $url = $url . urlencode($id);
-        
+
         User::find($id)->notify(new Survey($url));
         User::find($id)->update(["survey" => now()]);
 
